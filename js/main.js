681c64da57107e5715400bac6e46a7db468ccb95
Andrekode <93523910+Andrekode@users.noreply.github.com>
2022-03-11 12:12:25 +0100

    added func to hamburger menu and mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu")
+const mobileMenuLink = document.querySelectorAll(".mobile-menu-link")
+let menuOpen = false
+
+// functionlity to hamburger menu, hides and shows mobile menu version of nav. 
 
 menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
+  if (!menuOpen) {
+    menuBtn.classList.add('open')
+    mobileMenu.classList.add('menu-active')
+    menuOpen = true
+  }else {
+    menuBtn.classList.remove('open')
+    mobileMenu.classList.remove('menu-active')
+    menuOpen = false
+  }
   
 });
+
+// When user clicks nav link, returns hamburger menu to  default state and hides menu
+
+mobileMenuLink.forEach(element => {
+  element.addEventListener('click', () =>{
+    mobileMenu.classList.remove("menu-active")
+    menuBtn.classList.remove('open')
+    menuOpen = false
+  })
+})
